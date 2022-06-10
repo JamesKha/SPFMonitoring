c2b2e7a82773839e08675f24cf816ca691860b54
JamesKha <jimmy.kha@mytwu.ca>
2022-06-10 12:00:22 -0700

    Adding dropdown menus

--- UV.py
+++ UV.py
@@ -3,3 +3,23 @@ import pandas as pd
 import numpy as np
 
 st.title('Testing')
+
+st.text_input(label="Postal Code",  disabled=False)
+option = st.selectbox(
+     'Select Skin Type',
+     ('I', 'II', 'III', "IV", "V", "VI"))
+
+
+if option == "I": 
+    duration = 10
+elif option == "II": 
+    duration = 20 
+elif option == "III": 
+    duration = 30 
+elif option == "IV": 
+    duration = 50
+else:
+    duration = "More than 60"
+
+st.write("Skin Type:", option)
+st.write("You may stay outside for {} minutes".format(duration))
